458d01cf5dff4148038935021e65955a46803168
GreenJon902 <GreenJon902@gmail.com>
2021-08-05 19:35:05 +0100

    new button to add bars and stated custom cursor

--- app/ultimateDrumScorerProApp.py
+++ app/ultimateDrumScorerProApp.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.lang.builder import Builder
 
 from app.boxLayoutWithEvents import BoxLayoutWithHoverEvent, BoxLayoutWithClickHoverEvent
@@ -13,3 +14,18 @@ class UltimateDrumScorerProApp(App, ClassWithLogger):
         self.log_info("Built")
 
         return root
+
+
+    def set_cursor(self, name):
+        self.log_info(f"Setting cursor to {name}")
+
+        found = Window.set_system_cursor(name)
+
+        if not found:
+            Window.show_cursor = False
+
+            self.log_debug(f"No system cursor found for {name}, trying to find a custom one")
+
+        else:
+            Window.show_cursor = True
+            self.log_debug(f"System cursor found for {name}")
