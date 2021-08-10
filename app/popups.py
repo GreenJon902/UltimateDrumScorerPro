bfbe50fe94c0f69b5fcc282d21b3eba9379affbe
GreenJon902 <GreenJon902@gmail.com>
2021-08-10 11:23:00 +0100

    split popup class

--- app/popups.py
+++ app/popups.py
@@ -3,37 +3,19 @@ from kivy.uix.popup import Popup
 from logger.classWithLogger import ClassWithLogger
 
 
-class AddTextPopup(Popup, ClassWithLogger):
+class MyPopup(Popup, ClassWithLogger):
     def __init__(self, **kwargs):
-
-        text = kwargs.pop("text", None)
-        font_size = kwargs.pop("font_size", None)
-
         Popup.__init__(self, **kwargs)
         ClassWithLogger.__init__(self)
 
         self.register_event_type("on_cancelled")
         self.register_event_type("on_submitted")
 
-        if text:
-            self.ids["text"].text = text
-        if font_size:
-            self.ids["font_size"].value = font_size
-
     def get_entered(self):
-        array = {"text": self.ids["text"].text, "font_size": self.ids["font_size"].value}
-        self.log_dump(f"Data entered was requested, returning {array}")
-        return array
+        raise NotImplementedError("No get_entered method implemented")
 
     def on_finish_button(self, _instance, _value):
-        data = self.get_entered()
-
-        if data["text"] == "":
-            self.log_debug("Finish button clicked but no text entered so ignoring")
-
-        else:
-            self.dismiss(correct=True)
-            self.log_dump("Finish button clicked, dismissing self")
+        pass
 
 
     def dismiss(self, *args, correct=False, **kwargs):  # If correct is false then that means it was auto_dismissed
@@ -50,3 +32,34 @@ class AddTextPopup(Popup, ClassWithLogger):
 
     def on_submitted(self, data):
         pass
+
+
+
+class AddTextPopup(MyPopup):
+    def __init__(self, **kwargs):
+        text = kwargs.pop("text", None)
+        font_size = kwargs.pop("font_size", None)
+
+        MyPopup.__init__(self, **kwargs)
+
+        if text:
+            self.ids["text"].text = text
+        if font_size:
+            self.ids["font_size"].value = font_size
+
+
+    def get_entered(self):
+        array = {"text": self.ids["text"].text, "font_size": self.ids["font_size"].value}
+        self.log_dump(f"Data entered was requested, returning {array}")
+        return array
+
+
+    def on_finish_button(self, _instance, _value):
+        data = self.get_entered()
+
+        if data["text"] == "":
+            self.log_debug("Finish button clicked but no text entered so ignoring")
+
+        else:
+            self.dismiss(correct=True)
+            self.log_dump("Finish button clicked, dismissing self")
