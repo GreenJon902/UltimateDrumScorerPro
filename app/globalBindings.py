f3eea64605b0ff550e16013d22a64373dab72529
GreenJon902 <GreenJon902@gmail.com>
2021-08-15 17:12:14 +0100

    Cleanup | global bindings - added global bindings file

---
+++ app/globalBindings.py
@@ -0,0 +1,35 @@
+# Basics from https://github.com/GreenJon902/SpaceBuilderMergeGame/blob/master/lib/globalEvents.py
+
+
+logger = BetterLogger(name="GlobalEvents")
+
+
+class GlobalEvents:
+    bindings: dict[str, list] = {}
+
+    @classmethod
+    def bind(cls, **kwargs):
+        for event_name, function in kwargs.items():
+            cls.check_binding(event_name)
+
+            cls.bindings[event_name].append(function)
+            logger.log_deep_debug(function, "was bound to event '", event_name, "'")
+
+    @classmethod
+    def register(cls, event_name: str):
+        cls.bindings[event_name] = list()
+        logger.log_debug("Event '", event_name, "' has been created")
+
+    @classmethod
+    def dispatch(cls, event_name: str, *args, **kwargs):
+        cls.check_binding(event_name)
+        for function in cls.bindings[event_name]:
+            function(*args, **kwargs)
+
+    @classmethod
+    def check_binding(cls, event_name: str):
+        if event_name not in cls.bindings:
+            cls.register(event_name)
+
+
+__all__ = ["GlobalEvents"]
