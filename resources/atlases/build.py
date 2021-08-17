a975083d5ff775809321fd18e92ca26da257b831
GreenJon902 <GreenJon902@gmail.com>
2021-08-17 11:15:11 +0100

    Notes - changed build script, refactored some stuff and added 2 note heads

--- resources/atlases/build.py
+++ resources/atlases/build.py
@@ -5,27 +5,20 @@ from kivy.atlas import Atlas
 from kivy.logger import Logger
 
 padding = 5
-
+names_width = {"buttons": 3, "special_notes": 1, "note_heads": 1}
 
 if __name__ == '__main__':
-    Logger.info("AtlasMaker: Creating atlas for buttons")
+    for name in names_width.keys():
+        width = names_width[name]
 
-    button_files = [name for name in glob("../_buttons/*.png")]
-    button_files.sort()
-    single_size = Image.open(button_files[0]).size
-    multiple_size = int((single_size[0] * 3) + (padding * 3)), int((single_size[1] * len(button_files) / 3) + (padding * len(button_files) / 3))
+        Logger.info(f"AtlasMaker: Creating atlas for {name}")
 
-    Atlas.create("buttons", button_files, multiple_size, padding=padding)
+        button_files = [name for name in glob(f"../_{name}/*.png")]
+        button_files.sort()
+        single_size = Image.open(button_files[0]).size
+        multiple_size = int((single_size[0] * width) + (padding * width)), int(
+            (single_size[1] * len(button_files) / width) + (padding * len(button_files) / width))
 
-    Logger.info("AtlasMaker: Created atlas for buttons")
-    Logger.info("AtlasMaker: Creating atlas for notes")
+        Atlas.create(f"{name}", button_files, multiple_size, padding=padding)
+        Logger.info(f"AtlasMaker: Created atlas for {name}")
 
-    note_files = [name for name in glob("../_notes/*.png")]
-    note_files.sort()
-    single_size = Image.open(note_files[0]).size
-    multiple_size = int((single_size[0]) + padding), int(
-        (single_size[1] * len(note_files)) + (padding * len(note_files)))
-
-    Atlas.create("notes", note_files, multiple_size, padding=padding)
-
-    Logger.info("AtlasMaker: Created atlas for notes")
