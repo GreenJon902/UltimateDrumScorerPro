4ed32908b16f93470caaa05491ab0869479fe6d0
GreenJon902 <GreenJon902@gmail.com>
2021-08-20 16:48:35 +0100

    added sixteenth_rest

--- app_info/score_info.py
+++ app_info/score_info.py
@@ -4,4 +4,4 @@ note_duration_and_note_names_splitter_char = "-"
 note_name_splitter_char = " "
 
 note_name_to_staff_level = {"kick": 0, "snare": 2}
-duration_to_text_duration = {1: "quarter", 2: "eighth"}
+duration_to_text_duration = {1: "quarter", 2: "eighth", 4: "sixteenth"}
