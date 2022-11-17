42d0852ccc9e42962555116383bdb2bcc0856ff7
GreenJon902 <greenjon902@gmail.com>
2022-11-17 21:21:49 +0000

    added oval_with_tilted_line

--- src/config/config.py
+++ src/config/config.py
@@ -19,15 +19,15 @@ class NotePainElement(EventDispatcher):
 
 
 class Config:
-    notePains: list[list[NotePainElement]] = [
-        [
-            NotePainElement(pos=("0cm", "0cm"), size=("0.5cm", "0.25cm"), color=(0, 0, 0), symbol="tilted_line")
-        ]
-    ]
+    notePains: list[list[NotePainElement]] = [[
+        NotePainElement(pos=("0cm", "-1cm"), size=("0.5cm", "0.4cm"), color=(0, 0, 0), symbol="oval_with_tilted_line"), # Tom 1
+        NotePainElement(pos=("0cm", "-0.6cm"), size=("0.5cm", "0.4cm"), color=(0, 0, 0), symbol="oval_with_tilted_line"),  # Tom 2
+        NotePainElement(pos=("0cm", "0cm"), size=("0.5cm", "0.25cm"), color=(0, 0, 0), symbol="tilted_line"),  # Snare
+        NotePainElement(pos=("0cm", "0.4cm"), size=("0.5cm", "0.4cm"), color=(0, 0, 0), symbol="oval_with_tilted_line"),  # Tom 3
+    ]]
     currentNotePain = 0
 
-    line_thickness: int = metrics.cm(0.1)
-
+    line_thickness: int = metrics.cm(0.05)
 
 
 __all__ = ["Config"]
--- src/symbol.py
+++ src/symbol.py
@@ -1,5 +1,8 @@
+import math
+
+from betterLogger import ClassWithLogger
 from kivy import metrics
-from kivy.graphics import Line, PushMatrix, Scale, PopMatrix
+from kivy.graphics import Line, PushMatrix, Scale, PopMatrix, Ellipse, Rotate
 from kivy.properties import StringProperty
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.widget import Widget
@@ -7,14 +10,34 @@ from kivy.uix.widget import Widget
 from config.config import Config
 
 
-class Symbol(RelativeLayout):
+class Symbol(RelativeLayout, ClassWithLogger):
     def __init__(self, name, size, **kwargs):
         RelativeLayout.__init__(self, **kwargs)
+        ClassWithLogger.__init__(self)
 
         with self.canvas:
-            PushMatrix()
-
             if name == "tilted_line":
                 Line(points=(0, 0, -size[0], -size[1]), width=Config.line_thickness)
 
-            PopMatrix()
+            elif name == "oval_with_tilted_line":
+                Line(ellipse=(-size[0], -size[1]/2, size[0], size[1]), width=Config.line_thickness)
+
+                # Get the coordinates of where the points of the line y=0.5x+b/4 intersect with an ellipse with the size
+                # b by a
+                #
+                # ellipse equation is ((y ^ 2) / ((a / 2) ^ 2)) + (((x + (b / 2)) ^ 2) / ((b / 2) ^ 2)) = 1
+
+                a = size[1]
+                b = size[0]
+                x1 = (-2 * math.sqrt(a ** 2 * b ** 2 * (4 * a ** 2 + b ** 2)) - 4 * a ** 2 * b - b ** 3) / (
+                            2 * (4 * a ** 2 + b ** 2))
+                y1 = -math.sqrt(a ** 2 * b ** 2 * (4 * a ** 2 + b ** 2)) / (2 * (4 * a ** 2 + b ** 2))
+
+                x2 = (2 * math.sqrt(a ** 2 * b ** 2 * (4 * a ** 2 + b ** 2)) - 4 * a ** 2 * b - b ** 3) / (
+                            2 * (4 * a ** 2 + b ** 2))
+                y2 = math.sqrt(a ** 2 * b ** 2 * (4 * a ** 2 + b ** 2)) / (2 * (4 * a ** 2 + b ** 2))
+
+                Line(points=(x1, y1, x2, y2), width=Config.line_thickness)
+
+            else:
+                self.log_error(f"No symbol called \"{name}\"")
