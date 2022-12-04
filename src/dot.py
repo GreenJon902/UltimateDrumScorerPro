3a1c368a4f1d662a45f31e415b910463e5eb6679
GreenJon902 <greenjon902@gmail.com>
2022-12-04 10:34:17 +0000

    made Dot use CommittableButtonHandler

--- src/dot.py
+++ src/dot.py
@@ -8,28 +8,31 @@ from kivy.graphics import Color, Line
 from kivy.properties import NumericProperty, BooleanProperty
 from kivy.uix.widget import Widget
 
+from committableButtonHandler import CommittableButtonHandler
 from config.config import Config
 
 
 class Dot(Widget, ClassWithLogger):
     transparency: int = NumericProperty()
-    configurableness: int = NumericProperty()
     committed: bool = BooleanProperty()
-    hovered = False
+
+    configurableness: int = NumericProperty()
 
     def __init__(self, start_width=None, **kwargs):
-        self.current_hover = None
+        color = Color(rgb=(0, 0, 0))
+        self.dot = Line(width=Config.line_thickness)
+
+        CommittableButtonHandler.register(self, color, self.check_collision, "transparency", "committed",
+                                          "configurableness")
 
         Widget.__init__(self, **kwargs)
         ClassWithLogger.__init__(self)
+        self.canvas.add(color)
+        self.canvas.add(self.dot)
 
         self.trigger_poses = Clock.create_trigger(self._do_poses, -1)
         self.fbind("pos", self.trigger_poses)
-        self.fbind("pos", self.do_hovers)
-        self.fbind("configurableness", self.do_transparency)#
-        self.fbind("transparency", self.do_transparency)
-        self.fbind("committed", self.do_transparency)
-        Window.bind(mouse_pos=self.do_hovers)
+
 
         if start_width is None:
             self.width = Config.dot_x_spacing
@@ -38,55 +41,10 @@ class Dot(Widget, ClassWithLogger):
             a = Animation(width=Config.dot_x_spacing, duration=Config.dot_entrance_speed)
             a.start(self)
 
-        with self.canvas:
-            self.color = Color(rgb=(0, 0, 0))
-            self.dot = Line(width=Config.line_thickness)
-
-        self.do_transparency()
-
     def _do_poses(self, *_):
         self.dot.circle = (*self.pos, Config.dot_radius)
 
-    def do_transparency(self, *_):
-        self.color.a = (1 if self.committed else
-                        (Config.dot_selector_uncommitted_transparency * self.configurableness)) * self.transparency
-
-    def do_hovers(self, *_):
+    def check_collision(self, *_):
         pos = Window.mouse_pos
         distance = math.sqrt((pos[0] - self.x)**2 + (pos[1] - self.y)**2)
-        if distance <= Config.dot_selector_hover_radius:
-            if not self.hovered:
-                a = Animation(r=(Config.dot_selector_committed_hover_color[0] if self.committed else
-                                 Config.dot_selector_uncommitted_hover_color[0]),
-                              b=(Config.dot_selector_committed_hover_color[1] if self.committed else
-                                 Config.dot_selector_uncommitted_hover_color[1]),
-                              g=(Config.dot_selector_committed_hover_color[2] if self.committed else
-                                 Config.dot_selector_uncommitted_hover_color[2]),
-                              duration=Config.dot_selector_hover_fade_speed)
-                a.start(self.color)
-                self.hovered = True
-        elif self.hovered:
-            a = Animation(r=0,
-                          b=0,
-                          g=0,
-                          duration=Config.dot_selector_hover_fade_speed)
-            a.start(self.color)
-            self.hovered = False
-
-    def on_touch_up(self, touch):
-        pos = touch.pos
-        distance = math.sqrt((pos[0] - self.x) ** 2 + (pos[1] - self.y) ** 2)
-        if distance <= Config.dot_selector_hover_radius:
-            if self.committed:
-                self.committed = False
-            else:
-                self.committed = True
-
-            a = Animation(r=(Config.dot_selector_committed_hover_color[0] if self.committed else
-                             Config.dot_selector_uncommitted_hover_color[0]),
-                          b=(Config.dot_selector_committed_hover_color[1] if self.committed else
-                             Config.dot_selector_uncommitted_hover_color[1]),
-                          g=(Config.dot_selector_committed_hover_color[2] if self.committed else
-                             Config.dot_selector_uncommitted_hover_color[2]),
-                          duration=Config.dot_selector_hover_fade_speed)
-            a.start(self.color)
+        return distance <= Config.dot_selector_hover_radius, distance
