64126cf6a01f350ef4446cad9b98f4c096b1c99c
GreenJon902 <greenjon902@gmail.com>
2023-05-01 15:37:41 +0100

    editor cant be empty

--- src/editor/scoreSectionEditor/normalScoreSectionEditor_NoteEditor_PlusInbetween.py
+++ src/editor/scoreSectionEditor/normalScoreSectionEditor_NoteEditor_PlusInbetween.py
@@ -363,6 +363,8 @@ class NormalScoreSectionEditor_NoteEditor_PlusInbetween(NormalScoreSectionEditor
 
             if ry < note.height:
                 if nid in self.score_section_instance.score[index].note_ids:
+                    if len(self.score_section_instance.score) == 1:
+                        return  # Don't delete last section section
                     self.score_section_instance.score[index].note_ids.remove(nid)
                 else:
                     self.score_section_instance.score[index].note_ids.append(nid)
