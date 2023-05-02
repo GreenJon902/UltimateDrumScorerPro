3f177e5a62eb10283c8c3035646f8b993c7ba6bc
GreenJon902 <greenjon902@gmail.com>
2023-05-02 17:18:35 +0100

    made decorations scale decentlky in editor

--- src/score/decorations.py
+++ src/score/decorations.py
@@ -9,9 +9,12 @@ Builder.load_file("score/decorations.kv")
 class Decoration(RelativeLayout):
     color = ColorProperty(defaultvalue=(0, 0, 0, 1))
     min_height: int = NumericProperty()
-    container_height: int = NumericProperty()  # Height of container, e.g. the entire score
+    container_height: int = NumericProperty(allownone=True, defualtvalue=None)  # Height of container, e.g. the
+                                                                                # entire score
+    _container_height: float = NumericProperty()
 
     def __init__(self, **kwargs):
+        kwargs.setdefault("container_height", None)  # Ye so this is necessary.
         RelativeLayout.__init__(self, **kwargs)
         self.height = self.min_height
         self.canvas.opacity = 1
@@ -19,6 +22,13 @@ class Decoration(RelativeLayout):
     def on_height(self, _, value):
         if value < self.min_height:
             self.height = self.min_height
+        self.on_container_height(self, self.container_height)
+
+    def on_container_height(self, _, value):
+        if value is None:
+            self._container_height = self.height
+        else:
+            self._container_height = value
 
     def on_min_height(self, _, value):
         if self.height < value:
