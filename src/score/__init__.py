54815eb89caeca7718d8057548b0a518bcb825e1
GreenJon902 <greenjon902@gmail.com>
2023-05-06 12:20:11 +0100

    REFACTORING ONE - Moved all items to old folder

--- src/score/__init__.py
+++
@@ -1,6 +0,0 @@
-from score.decorations import Decoration
-from score.notes import Note
-from score.storage import *
-from score.storage.scoreSectionStorage import ScoreSectionStorage, ScoreSectionSectionStorage, \
-    fix_and_get_normal_editor_note_ids
-from score.storage.textStorage import TextStorage
