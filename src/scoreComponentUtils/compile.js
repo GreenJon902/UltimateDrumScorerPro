a1176243eb9dbd6b4706bdd73609e877ecb1773b
GreenJon902 <greenjon902@gmail.com>
2025-12-26 19:55:03 +0000

    Finished compiler for score components

---
+++ src/scoreComponentUtils/compile.js
@@ -0,0 +1,330 @@
+import {ComponentManager} from "../componentManager.js";
+
+class RenderInstruction {
+    // Render instructions are produced from data in the ComponentManager, and dictate exactly what needs to be drawn. 
+    // They allow the actual drawing function to ignore the calculations required for beams, etc.
+    //
+    // There are a couple types of render-instructions (and what they store):
+    //     BEAM: 
+    //         - drumIDs
+    //         - full-beams
+    //         - broken-beams
+    //         - dots
+    //         - length
+    //     BEAM-END:
+    //         - drumIDs
+    //         - dots
+    //         - length
+    //     FLAG:
+    //         - drumIDs
+    //         - flags
+    //         - dots
+    //         - length
+    //     REST:
+    //         - ticks
+    //         - dots
+    //         - length
+    //     CONTRACT-START:
+    //         - ratio
+    //         - hooks
+    //     CONTRACT-END:
+    //     DECORATION:
+    //         - decorationID
+    // 
+    // drumIDs: A string array of the (drum) symbolIDs to draw.
+    // full-beams: The number (non-zero and positive) of full beams to draw between this instruction and the next instruction (with a stem, full beams go over rests).
+    // broken-beams: The same full-beams except for broken-beams. This can be signed, where negative means to draw on the left, and positive to the right. It can also be zero - no broken-beams. If dots != 0 then this cannot be negative.
+    // dots: The number (zero or positive) of dots that should be drawn after the stem. If broken-beams is negative (broken-beams on the left) then this must be 0 (no dots).
+    // flags: The number (zero or positive) of flags to draw after the stem.
+    // ticks: The number (zero or positive) of ticks to draw on a rest. Zero means it's a crotchet rest.
+    // ratio: The length (positive integer) of notes to contracted into one beat. This is the number to be drawn between the start and end.
+    // hooks: Should hooks (the lines that show where a contraction has effect) be drawn. This is true or false.
+    // length: The relative duration of a note compared to the rest of the notes. If a note is twice as long then it should have double the duration.
+    // decoration:  The (decoration) symbolID of the decoration to draw. 
+    // 
+    // BEAMs connect to the next BEAM or BEAM-END, so must be followed by at least one of these.
+    // A BEAM-END must follow a BEAM.
+    // FLAGs stand alone so should not follow an un-ended BEAM. This means crotchets should be represented using a FLAG with flags=0.
+    // Each CONTRACT-START must be closed by a CONTRACT-END, and must be closed before another CONTRACT can start.
+    // RESTS and CONTRACTING-START/END can come anywhere between BEAMs and FLAGs.
+    // DECORATIONs cannot come within unended beams.
+    // 
+    // CONTRACT beams are for contracting-ratios, they say notes inside this beam (of the length given inside ratio) should be contracted so that they last the length of a single beat.
+    // 
+    //
+    // We refer to instructions with a length - BEAMs, BEAM_ENDs, FLAGs, and RESTs - as groups as they are groups of subdivisions.
+    
+    // Instruction Types
+    static get BEAM() {return "BEAM";}  // Declare like this so are immutable.
+    static get BEAM_END() {return "BEAM_END";}
+    static get FLAG() {return "FLAG";}
+    static get REST() {return "REST";}
+    static get CONTRACT_START() {return "CONTRACT_START";}
+    static get CONTRACT_END() {return "CONTRACT_END";}
+    static get DECORATION() {return "DECORATION";}
+
+    constructor(type, ...args) {
+        // Type should be the value in BEAM, BEAM_END...
+        // Args should be passed in the order they are documented in.
+        
+        this.type = type;
+        if (type === RenderInstruction.BEAM) {
+            this.drums = args[0];
+            this.fullBeams = args[1];
+            this.brokenBeams = args[2];
+            this.dots = args[3];
+            this.length = args[4];
+        } else if (type === RenderInstruction.BEAM_END) {
+            this.drums = args[0];
+            this.dots = args[1];
+            this.length = args[2];
+        } else if (type === RenderInstruction.FLAG) {
+            this.drums = args[0];
+            this.flags = args[1];
+            this.dots = args[2];
+            this.length = args[3];
+        } else if (type === RenderInstruction.REST) {
+            this.ticks = args[0];
+            this.dots = args[1];
+            this.length = args[2];
+        } else if (type === RenderInstruction.CONTRACT_START) {
+            this.ratio = args[0];
+            this.hooks = args[1];
+        } else if (type === RenderInstruction.CONTRACT_END) {
+        } else if (type === RenderInstruction.DECORATION) {
+            this.decoration = args[0];
+        } else {
+            throw "Unkown type " + type;
+        }
+
+        Object.freeze(this);  // So is immutable
+    }
+}
+
+function makeGroupLengthsLegal(length) {
+    // For a group length to be legal (i.e. it possible to be notated without the use of (more) rests), we need it to be able to be written as the sum of consecutive powers of 2.
+    // E.g. 4 = 4               just a beam or flag or nothing,
+    //      3 = 2 + 1           one dot,
+    //      6 = 4 + 2           one dot,
+    //      7 = 4 + 2 + 1       two dots,
+    //      but 9 = 8 + 1       does not work; this would need to be followed by a rest.
+    //      And 21 = 16 + 4 + 1 needs two rests.
+    
+    const legalLengths = new Array();
+    
+    while (length != 0) {
+        // Each iteration, find the largest series `2^k + 2^(k+1) + ... + 2^n` that fits into length
+        
+        // 2^n is the largest power of two in `length`, so we can floor the log of `length` to find `n`
+        const n = Math.floor(Math.log2(length));
+        
+        // Our series simplifies to `2^(n+1) - 2^k`, we can use this to find `k` by equating it to length
+        const k = Math.ceil(Math.log2(Math.pow(2, n+1) - length))
+        
+        // Then get the length of the series (the legal length of the group we are added (the missing part will be handled in later iterations))
+        const series = Math.pow(2, n+1) - Math.pow(2, k)
+        legalLengths.push(series);
+        // Subtract the series from length as we have accounted for it
+        length -= series;
+    }
+    
+    return Object.freeze(legalLengths);
+}
+
+function groupSubdivisions(componentId, beatI) {
+    // Returns the number of subdivisions that each group (an instruction with length) will take for the given component and beat.
+    // This will determine how many groups are required, and split lengths to ensure they can be legally beamed/notated (i.e. this calculates where extra rests are needed).
+    // 
+    // It returns a frozen array of integers which are the lengths. Each corresponds to a subdivision, but not each subdivision has a length:
+    //      The first length - let this be `a` - corresponds to the 0th subdivision.
+    //      The second length - `b` - corresponds to the `a`th subdivison.
+    //      The third to the `a+b`th subdivison and so on.
+    // If a subdivison is empty then it should have a rest, otherwise it should be a group.
+    // The lengths will be such that all subdivisions with drums have a length assigned.
+    
+    const groupLengths = new Array();
+    
+    let subdivI = 0;
+    const subdivCount = ComponentManager.getComponentBeatSubdivisionCount(componentId, beatI);
+    while (subdivI < subdivCount) {
+        
+        // In each iteration we find the length of the group starting at subdivI
+
+        const groupStartSubdivI = subdivI;
+        subdivI++;
+        
+        // Loop until subdivI is at the start of the next group, or at the end of the beat
+        while (subdivI < subdivCount && ComponentManager.getComponentSubdivisionDrums(componentId, beatI, subdivI).size == 0) {
+            subdivI++;
+        }
+        
+        // So we have a group starting at groupStartSubdivI and ending on (subdivI-1)
+        
+        const groupLength = subdivI - groupStartSubdivI
+        const legalGroupLengths = makeGroupLengthsLegal(groupLength)
+        
+        groupLengths.push(...legalGroupLengths);
+    }
+    
+    return Object.freeze(groupLengths);
+}
+
+function calculateRyhthmInfo(groupLength, subdivisionCount) {
+    // Calculates the rhythm information for a group of the given length inside a beat with the given number of subdivisions.
+    // This returns {
+    //                  beams: int,  // The number of beams/ticks/flags this groups requires attached to its stem
+    //                  dots: int    // The number of dots that follow the stem
+    //              }
+    // 
+    // This expects the group length to be a legal length, i.e. it can be written as the sum of consecutive powers of two.
+    
+    // If we have a non-standard number of subdivisions (not a power of 2) then we notate as if the subdivisions was the highest power of two below what it actually is
+    const pretendSubdivisionsCount = Math.pow(2, Math.floor(Math.log2(subdivisionCount)));
+    const logPsc = Math.log2(pretendSubdivisionsCount)
+
+    // If `groupLength = 2^n + 2^(n-1) + ... + 2^k`, then `logPsc - n` is the number of beams, and `(n - k)` is the number of dots
+    const n = Math.floor(Math.log2(groupLength))  // `2^n` is the highest power so this works
+    const k = Math.ceil(Math.log2(Math.pow(2, n+1) - groupLength))  // As the series evaluates to `2^(n+1) - 2^k`
+    
+    return Object.freeze({beams: logPsc - n, dots: n - k});
+}
+
+function getNonEmptySubdivisions(componentId, beatI) {
+    // Gets the subdivision-indexes of non-empty (have drums) subdivisions in a given beat and component.
+    
+    const subdivCount = ComponentManager.getComponentBeatSubdivisionCount(componentId, beatI);
+    const nonEmpties = new Array();
+    for (let subdivI = 0; subdivI<subdivCount; subdivI++) {
+        if (ComponentManager.getComponentSubdivisionDrums(componentId, beatI, subdivI).size != 0) {
+            nonEmpties.push(subdivI);
+        }
+    }
+    
+    return Object.freeze(nonEmpties);
+}
+
+
+function calculateBeamInfo(l, c, n, nn) {
+    // Calculates how many full and broken beams are needed between `c` and `n`. Where `l`, `c`, `n`, and `nn` are the beams required connected to the stems.
+    // Returns {fullBeams: int, brokenBeams: int}.
+    
+    let fullBeams, brokenBeams;
+    if (c == n) {  // Both want the same number of beams
+        fullBeams = c;
+        brokenBeams = 0;
+    } else if (c > n && l >= c) {  // c needs more beams than n, but l supplies at least that many to c
+        fullBeams = n;
+        brokenBeams = 0;
+    } else if (c > n && l < c) {  // c needs more beams than n, and l does not supply enough to c
+        fullBeams = n;
+        brokenBeams = -(n - c);  // Negative as beams on left side
+    } else if (c < n && nn >= n) {  // c needs less beams than n, but nn supplies at least that many to n
+        fullBeams = c;
+        brokenBeams = 0;
+    } else if (c < n && nn < n) {  // c needs less beams than n, but nn does not supply enough beams to n
+        fullBeams = c;
+        brokenBeams = n - c;  // Positive as beams on right side
+    } else {
+         throw "None of the beam logic cases worked, this shouldn't be possible, here are the values " + l + " " + c + " " + n + " " + nn;
+    }
+    
+    return {fullBeams: fullBeams, brokenBeams: brokenBeams};
+}
+
+// TODO: RenderInstruction length arguement needs to take into account all beats in bar
+// TODO: Simplify contraction ratio
+
+function createGroupInstructions(componentId, beatI) {
+    // Creates the group instructions for a given beat for a given component.
+    
+    // Get some preliminary data
+    const groupLengths = groupSubdivisions(componentId, beatI);
+    const expandedGroupLengths = groupLengths.map(l => new Array(l).fill(l)).reduce((a, b) => a.concat(b));  // Has an item for each subdivision containing the length of its group
+    const nonEmptySubdivisions = getNonEmptySubdivisions(componentId, beatI);
+    const nonEmptyCount = nonEmptySubdivisions.length;
+    const subdivCount = ComponentManager.getComponentBeatSubdivisionCount(componentId, beatI);
+    
+    // Create the instructions. We need one for each group length
+    const instructions = new Array();
+    let subdivI = 0;
+    let countedNonEmpty = 0;  // Number of non-empties we have processed so far
+    let inBeamFlag = false;  // Is there no BEAM instruction || Was a BEAM_END more recent than a BEAM
+    for (let groupI = 0; groupI < groupLengths.length; groupI++) {
+        const drums = ComponentManager.getComponentSubdivisionDrums(componentId, beatI, subdivI);
+        const groupLength = groupLengths[groupI];
+        const rhythmInfo = calculateRyhthmInfo(groupLength, subdivCount);
+        
+        if (drums.size == 0) {
+            // We have a REST
+            instructions.push(new RenderInstruction(RenderInstruction.REST, rhythmInfo.beams, rhythmInfo.dots, groupLength));
+            
+        } else if (countedNonEmpty < nonEmptyCount - 1 && (rhythmInfo.beams > 0 && calculateRyhthmInfo(expandedGroupLengths[nonEmptySubdivisions[countedNonEmpty + 1]], subdivCount).beams > 0)) {  // If at least one more nonEmpty after this, and both require beams
+            // We have a BEAM
+            
+            // How many beams do the adjacent groups each supply / need:
+            const l = ((countedNonEmpty == 0) ? 0 : calculateRyhthmInfo(expandedGroupLengths[nonEmptySubdivisions[countedNonEmpty - 1]], subdivCount).beams);
+            const c = rhythmInfo.beams;
+            const n = (calculateRyhthmInfo(expandedGroupLengths[nonEmptySubdivisions[countedNonEmpty + 1]], subdivCount).beams);  // We can always have the  + 1 as otherwise it won't get into this case
+            const nn = ((countedNonEmpty == nonEmptyCount - 2) ? 0 : calculateRyhthmInfo(expandedGroupLengths[nonEmptySubdivisions[countedNonEmpty + 2]], subdivCount).beams);
+            
+            // Calculate the beam info
+            const {fullBeams: fullBeams, brokenBeams: brokenBeams} = calculateBeamInfo(l, c, n, nn);
+            
+            instructions.push(new RenderInstruction(RenderInstruction.BEAM, drums, fullBeams, brokenBeams, rhythmInfo.dots, groupLength));
+            countedNonEmpty++;
+            inBeamFlag = true;
+            
+        } else if (inBeamFlag) {
+            // We have a BEAM_END
+            instructions.push(new RenderInstruction(RenderInstruction.BEAM_END, drums, rhythmInfo.dots, groupLength))
+            countedNonEmpty++;
+            inBeamFlag = false;
+            
+        } else {
+            // We have a FLAG
+            instructions.push(new RenderInstruction(RenderInstruction.FLAG, drums, rhythmInfo.beams, rhythmInfo.dots, groupLength));
+            countedNonEmpty++;
+        }
+        
+        subdivI += groupLength;
+    }
+    
+    return Object.freeze(instructions);
+}
+
+export function compileScoreComponent(componentId) {
+    // Converts the given score-component into a frozen array of RenderInstructions.
+    // This implementation will take each beat on its own, so will not beam between beats.
+    
+    const instructions = new Array();
+    
+    // Left-Decoration
+    const leftDeco = ComponentManager.getComponentLeftDecoration(componentId);
+    if (leftDeco !== null) instructions.push(new RenderInstruction(RenderInstruction.DECORATION, leftDeco));
+
+    // Convert actual note information to instructions
+    const beatCount = ComponentManager.getComponentBeatCount(componentId)
+    for (let beatI=0; beatI<beatCount; beatI++) {
+        const subdivCount = ComponentManager.getComponentBeatSubdivisionCount(componentId, beatI);
+        
+        // Get instructions for beat
+        const beatInstructions = createGroupInstructions(componentId, beatI);
+        
+        // Do we need a contract
+        const needsContract = Math.log2(subdivCount) % 1 !== 0;
+        const needsHooks = beatInstructions.filter(instr => instr.type === RenderInstruction.FLAG).length !== 0 ||
+            beatInstructions[0].type === RenderInstruction.REST ||
+            beatInstructions[beatInstructions.length - 1].type === RenderInstruction.REST;
+        
+        // Add instructions to list
+        if (needsContract) instructions.push(new RenderInstruction(RenderInstruction.CONTRACT_START, subdivCount, needsHooks));
+        instructions.push(...beatInstructions);
+        if (needsContract) instructions.push(new RenderInstruction(RenderInstruction.CONTRACT_END));
+    }
+    
+    // Right-Decoration
+    const rightDeco = ComponentManager.getComponentRightDecoration(componentId);
+    if (rightDeco !== null) instructions.push(new RenderInstruction(RenderInstruction.DECORATION, rightDeco));
+    
+    console.log(instructions);
+}
