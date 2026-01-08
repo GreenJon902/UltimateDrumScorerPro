1a8d0adb66c9d7dcaf08ccc09cee839c3f6b5d29
GreenJon902 <greenjon902@gmail.com>
2026-01-08 18:28:27 +0000

    Fixed issue with broken-beam decision making where broken beams were placed on both sides of a stem

--- src/scoreComponentUtils/compile.js
+++ src/scoreComponentUtils/compile.js
@@ -224,13 +224,23 @@ function calculateBeamInfo(l, c, n, nn) {
         brokenBeams = 0;
     } else if (c > n && l < c) {  // c needs more beams than n, and l does not supply enough to c
         fullBeams = n;
-        brokenBeams = n - c;  // Will be negative (as beams on left side)
+        // Supply broken beams if there will be more total beams between l and c than between c and n
+        if (l < n) {
+            brokenBeams = n - c;  // Will be negative (as beams on left side)
+        } else { 
+            brokenBeams = 0;
+        }
     } else if (c < n && nn >= n) {  // c needs less beams than n, but nn supplies at least that many to n
         fullBeams = c;
         brokenBeams = 0;
     } else if (c < n && nn < n) {  // c needs less beams than n, but nn does not supply enough beams to n
         fullBeams = c;
-        brokenBeams = n - c;  // Will be positive (as beams on right side)
+        // Supply broken beams if there will be more total beams between c and n than between n and nn
+        if (c > nn) {
+            brokenBeams = n - c;  // Will be positive (as beams on right side)
+        } else {
+            brokenBeams = 0;
+        }
     } else {
          throw "None of the beam logic cases worked, this shouldn't be possible, here are the values " + l + " " + c + " " + n + " " + nn;
     }
